6ec310f5678ff9287e2b77ded7ced22328675e2e
lincxx <bossc@BOSSCO>
2023-07-13 06:16:00 -0400

    finished

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+       <div>
+        <div className='mid-gray f3'>
+            {'Jeremy, your current rank is...'}
+        </div>
+        <div className='mid-gray f1'>
+            {'#5'}
+        </div>
+       </div>
+    )
+}
+
+export default Rank;
